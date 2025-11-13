6ef90e2eff314ba5e7f689583bd516094c2e079c
boreq <boreq@0x46.net>
2025-11-13 15:57:30 +0100

    Fix video makes the content jump when it loads

--- src/components/DashboardVideo.vue
+++ src/components/DashboardVideo.vue
@@ -1,9 +1,14 @@
 <template>
   <Window class="dashboard-video">
     <WindowContent>
-      <video autoplay loop muted>
-         <source src="video.mp4" type="video/mp4">
+      <video autoplay loop muted ref="video">
+        <source src="video.mp4" type="video/mp4">
       </video>
+      <div class="loading" v-if="!playing">
+        <SlowBlink>
+          RENDERING SIMULATION
+        </SlowBlink>
+      </div>
     </WindowContent>
   </Window>
 </template>
@@ -12,20 +17,40 @@
 import { defineComponent } from 'vue';
 import Window from '@/components/Window.vue';
 import WindowContent from '@/components/WindowContent.vue';
+import SlowBlink from '@/components/SlowBlink.vue';
 
 export default defineComponent({
   name: 'DashboardVideo',
   components: {
     Window,
     WindowContent,
+    SlowBlink,
+  },
+  mounted() {
+    window.addEventListener('resize', this.resizeVideo);
+    this.resizeVideo();
+  },
+  unmounted() {
+    window.removeEventListener('resize', this.resizeVideo);
+  },
+  methods: {
+    resizeVideo(): void {
+      const videoWidth = 1280;
+      const videoHeight = 720;
+      const video = this.$refs.video as HTMLVideoElement;
+      const height = (video.clientWidth * videoHeight) / videoWidth;
+      video.style.height = `${height}px`;
+    },
   },
 });
 </script>
 
 <style scoped lang="scss">
-video {
-  width: 100%;
-  height: 100%;
-  object-fit: contain;
+.dashboard-video {
+  video {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
 }
 </style>
